42c778cab9a872772090d1c53dc3a45efead543c
Andrew Stone <andrew@visitingmedia.com>
2023-12-01 16:07:48 -0800

    forked, cleaned up, added part 1 and part 2 of day 1

--- 2023/day01.js
+++ 2023/day01.js
@@ -1,57 +1,57 @@
 const NOPE = Symbol("NOPE");
+
 const cleanRawInput = (raw) => raw.trim().split(/\n/);
 
-const numbers = [
-  "one",
-  "two",
-  "three",
-  "four",
-  "five",
-  "six",
-  "seven",
-  "eight",
-  "nine",
-];
-const rNumbers = new RegExp(`^(?:${numbers.join("|")})`);
+const numberMap = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
 
-const solution = (input) =>
+const solution1 = (input) =>
   input
-    .map((line) => line.match(/\d/g) || [])
-    .map((list) => [list.at(0), list.at(-1)].join(""))
-    .map(Number)
-    .reduce((a, b) => a + b);
+    .map((code) => {
+      var num = code.replace(/\D/g, "");
+      return num.length && `${num.at(0)}${num.at(-1)}`;
+    })
+    .filter((code) => code)
+    .reduce((acc, a) => acc + parseInt(a), 0);
 
-function parse(str) {
-  const result = str.split("").reduceRight((acc, char, index) => {
-    const [found] = str.slice(index).match(rNumbers) ?? [];
+const solution2 = (input) =>
+  input.map((code) => {
+    const regexp = `(?=(${Object.keys(numberMap).join('|')}))`;
+    const set = [...code.matchAll(regexp)];
+    let count = 0;
+    set.forEach((match) => {
+      const value = match[1];
+      const index = match.index + count;
+      count += numberMap[value].toString().length;      
+      code = code.substring(0, index) + numberMap[value] + code.substring(index);
+    })
 
-    if (found) {
-      return `${numbers.indexOf(found) + 1}` + acc;
-    } else if (!isNaN(+char)) {
-      return char + acc;
-    }
-
-    return acc;
-  }, "");
-
-  return result;
-}
+    return code;
+  });
 
 function partOne(input, report, answer) {
-  const result = solution(input);
-
+  const result = solution1(input);
   report("Part one", result, answer);
 }
 
 function partTwo(input, report, answer) {
-  const result = solution(input.map(parse));
-
+  const result = solution1(solution2(input));
   report("Part two", result, answer);
 }
 
 module.exports = (raw, { report }) => {
   const input = cleanRawInput(raw);
 
-  partOne(input, report, 54968);
-  partTwo(input, report, 54094);
+  partOne(input, report, 53974);
+  partTwo(input, report, 52840);
 };
+
